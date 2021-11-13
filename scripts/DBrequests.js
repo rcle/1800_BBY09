df2e8407a492fa52023ece7f90cafc33df5b2655
Ryan Lee <ryanc_lee@hotmail.com>
2021-11-13 11:20:49 -0800

    added a separate js file for database functions, also implemented reading user name from db and showing in main.html

---
+++ scripts/DBrequests.js
@@ -0,0 +1,47 @@
+/*
+Gets name name stored in name field from user
+
+input: none
+output:string of Name or undefined
+*/
+function getName(){
+    firebase.auth().onAuthStateChanged((user) => {
+    if (user) {
+        //gets user id
+        const uid = user.uid;
+
+        /*checks the db collection users, and gets the doc which
+         *contains all the information stored of the current user
+         */
+        db.collection("users").doc(uid).get().then( function (doc){
+            document.getElementById("name").innerHTML = doc.data().name;
+        });
+    } else {
+        console.log("no user logged in");
+    }
+    });
+}
+
+/*
+Gets favourite data from user, not implemented yet, but to be used in the
+favourites page to populate the data within it
+
+inpupt: none
+output:array of favourites or undefined
+*/
+function getFavourite(){
+
+    firebase.auth().onAuthStateChanged((user) => {
+    if (user) {
+        const uid = user.uid;
+        db.collection("users").doc(uid).get().then( function (doc){
+            console.log(doc.data().name);
+            document.getElementById("favourite").innerHTML = doc.data().name;
+
+        });
+
+    } else {
+        console.log("no user logged in");
+    }
+    });
+}
